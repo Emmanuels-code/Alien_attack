d2e18cb68d958986b4a834a92e7cf278497fa32b
Emmanuels-code <emmaokogho@yahoo.com>
2024-10-05 22:03:43 +0000

    ship and alien attack

---
+++ js/utils.js
@@ -0,0 +1,6 @@
+function createAlien() {
+    const hull = Math.floor(Math.random() * (6 - 3 + 1)) + 3;
+    const firepower = Math.floor(Math.random() * (4 - 2 + 1)) + 2;
+    const accuracy = Math.random() * (0.8 - 0.6) + 0.6;
+    return new Ship(hull, firepower, accuracy);
+}
